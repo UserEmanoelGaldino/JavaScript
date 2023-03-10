15ef3b53346741557e06ee392a041f112f48bba3
Emanoel Galdino <emanoel.galdino.cardoso@gmail.com>
2023-03-10 18:31:37 -0300

    Atualização
    Teste de alert, confirm e prompt

---
+++ aula04/ex001.js
@@ -0,0 +1,10 @@
+
+/*Primeira maneira de colocar esses três comandos:*/
+window.alert('Minha primeira mensagem') 
+window.confirm('Está gostando de JavaScript?')
+window.prompt('Informe seu nome!')
+
+/*Segunda maneira, sem window:*/
+alert('Teste de segunda mensagem')
+confirm('Deu certo?')
+prompt('E qual é o seu segundo nome ou sobrenome?')
