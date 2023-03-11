b3e6cc70192466cefc4a811b6e4e48a7f8ba6bc8
Emanoel Galdino <emanoel.galdino.cardoso@gmail.com>
2023-03-11 00:14:42 -0300

    Aula 05
    Adicionado o conteúdo da aula 05 e realizado um ajuste no conteúdo da aula 04

--- aula04/ex001.js
+++ aula04/ex001.js
@@ -1,10 +1,12 @@
 
-/*Primeira maneira de colocar esses três comandos:*/
+/*
+Primeira maneira de colocar esses três comandos:
+*/
 window.alert('Minha primeira mensagem') 
 window.confirm('Está gostando de JavaScript?')
 window.prompt('Informe seu nome!')
 
-/*Segunda maneira, sem window:*/
+//Segunda maneira, sem window:
 alert('Teste de segunda mensagem')
 confirm('Deu certo?')
 prompt('E qual é o seu segundo nome ou sobrenome?')
