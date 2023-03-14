2ce75400a6c36aa18fbd3bcf0c154b93d8e0a88a
Emanoel Galdino <emanoel.galdino.cardoso@gmail.com>
2023-03-14 00:43:03 -0300

    Atualização Js

---
+++ aula06/js/ex002.js
@@ -0,0 +1,4 @@
+var nome = prompt('Qual o seu nome?') // Pergunta o nome do cliente;
+alert('bem vindo, ' + nome + '.') // Texto concatenado com variável nome;
+
+ 
---
+++ aula06/js/ex003.js
@@ -0,0 +1,5 @@
+// Coleta de informações:
+n1 = prompt('Informe o primeiro número!')
+n2 = prompt('Informe o segundo número!')
+
+// tratamento do dado:
